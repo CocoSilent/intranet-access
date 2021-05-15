be431129a79273423874bc88b37197fcbd107f8b
xsl <xiaosailun@163.com>
2021-05-15 22:29:28 +0800

    docker更新

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
 
+if [ ! ${$ADDRESS1} ]; then
+  echo -e "没有配置要访问的Ip地址的环境变量ADDRESS1 ...\n"
+  exit 1
+fi
+
 
 echo -e "\n========================1. 更新源代码========================\n"
 [ ! -d ${WORK_DIR}/log ] && mkdir -p ${WORK_DIR}/log
 bash git_pull
 echo
 
-
-
 echo -e "容器启动成功...\n"
+sed "5c proxy_pass  $ADDRESS1;" /etc/nginx/conf.d/address1.conf
 
 nginx
 
